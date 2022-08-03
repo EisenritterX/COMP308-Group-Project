42b1b23257b395f5ad12c9717320e7b0b8f1fe5a
George Zhou <georgezhou89@hotmail.com>
2022-08-03 17:45:08 -0400

    added config, graphql schemas and models

---
+++ server/models/SymptomList.js
@@ -0,0 +1,10 @@
+// Load the Mongoose module and Schema object
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const SymptomListSchema = new Schema({
+
+
+
+});
+module.exports = mongoose.model('Symptom', SymptomListSchema)
