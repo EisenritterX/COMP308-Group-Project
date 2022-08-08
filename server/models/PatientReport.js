7c2ad99557fa0b32767c904e69d11a428655d9ac
George Zhou <georgezhou89@hotmail.com>
2022-08-08 19:54:00 -0400

    Updated Querys & report model

--- server/models/PatientReport.js
+++ server/models/PatientReport.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PatientReport = new Schema({
-
-
-
+    id: String,
+    patientId: String,
+    nurseId: String,
+    bodyTemperature: Number,
+    heartRate: Number,
+    bloodPressure: Number,
+    respiratoryRate:Number,
+    dateFiled:Date
 });
 module.exports = mongoose.model('PatientReport', PatientReportSchema)
