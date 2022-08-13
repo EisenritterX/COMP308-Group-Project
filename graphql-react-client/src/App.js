28459b8febb49f83f24cfdf5c95ab729155eecab
George Zhou <georgezhou89@hotmail.com>
2022-08-13 19:30:56 -0400

    Added Home Page

--- graphql-react-client/src/App.js
+++ graphql-react-client/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+//
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Routes
+} from "react-router-dom";
+//
+// This app requires react-bootstrap and bootstrap installed: 
+//  npm install react-bootstrap bootstrap
+//
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import './App.css';
+// ADD COMPONENTS HERE
+// import StudentList from './components/StudentList';
+// import AddStudent from './components/AddStudent';
+// import EditStudent from './components/EditStudent';
+// import DeleteStudent from './components/DeleteStudent';
 
+import Home from './components/Home';
+
+//
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      
+      <Navbar bg="primary" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="home">COMP308 Group Project</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/home" >Home</Nav.Link>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path = "home" element={<Home />} /> 
+
+        </Routes>
     </div>
+      
+      
+
+    </Router>
+
+
   );
 }
-
+//<Route render ={()=> < App />} path="/" />
 export default App;
