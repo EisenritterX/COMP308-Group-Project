0516509b4f9d22da6425215f2ffc1f2f2d51e819
abishekh-manoharan <110298717+abishekh-manoharan@users.noreply.github.com>
2022-08-13 15:02:30 -0400

    Filled empty models, imported models in gql schema

--- server/models/EmergencyAlert.js
+++ server/models/EmergencyAlert.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const EmergencyAlertSchema = new Schema({
-
-
-
+    id: String,
+    patientId: String,
+    responderId: String,
+    nurseId: String
 });
 module.exports = mongoose.model('EmergencyAlert', EmergencyAlertSchema)
--- server/models/SymptomList.js
+++ server/models/SymptomList.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const SymptomListSchema = new Schema({
-
-
-
+    id: String,
+    symptom: String,
+    weight: Number
 });
 module.exports = mongoose.model('Symptom', SymptomListSchema)
